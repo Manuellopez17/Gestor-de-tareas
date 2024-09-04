72145316d22d1de04d2e06dba6a7468003537b11
Manuel <carloszuniga2038@gmail.com>
2024-09-04 10:40:44 -0500

    proyecto finalizado

--- creacion/models.py
+++ creacion/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Tarea(models.Model):
+    estados = [('completo','Finalizado'),('incompleto','Pendiente')]
+    titulo = models.CharField(max_length=100)
+    descripcion = models.TextField(max_length=500)
+    estado = models.CharField(max_length=50,choices=estados,default='incompleto')
+    def __str__(self):
+        return self.titulo
+    
---
+++ creacion/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('crear', crear, name='crear'),
+    path('|', listar, name='listar')]
